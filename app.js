dd58ca94b89966f8397595e4cc6806f40d22bb0b
Brendan Lewis <lewis2ba@dukes.jmu.edu>
2015-12-02 16:20:25 -0500

    added dependencies and root route to app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require("express");
+var app = express();
+var path = require("path");
+var bodyParser = require("body-parser");
+
+app.use(express.static(path.join(__dirname,"/public")));
+app.use(bodyParser.json());
+
+app.listen(7812,function(){
+  console.log("Listening on port 3000");
+});
+
+app.get("/", function(req, res){
+  res.send("Hello World")
+});
