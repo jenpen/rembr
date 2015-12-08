ba28ac71490427cb9cf55f899788a83a8d0a01a8
jen pengelly <jgpengelly@gmail.com>
2015-12-07 20:58:12 -0500

    updated map with popup example including map coordinates.

--- public/js/script.js
+++ public/js/script.js
@@ -16,7 +16,16 @@ $(document).ready(function(){
     // perspective = new Perspective(some JSON)
 
     L.marker([e.latlng.lat, e.latlng.lng]).addTo(map);
-
-
   });
+
+  var popup = L.popup();
+
+  function onMapClick(e) {
+    popup
+    .setLatLng(e.latlng)
+    .setContent("You clicked the map at " + e.latlng.toString())
+    .openOn(map);
+  }
+
+  map.on('click', onMapClick);
 });
