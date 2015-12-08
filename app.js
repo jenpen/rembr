3f9d5f0ee1895a1d4ef5c076e0c37f92f1728251
jen pengelly <jgpengelly@gmail.com>
2015-12-08 11:22:11 -0500

    refactor updates to app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var bodyParser   = require('body-parser');
 var session      = require('express-session');
 var perspectivesController = require('./controllers/perspectivesController');
 var usersController = require('./controllers/usersController');
-// var api_key = require("./config/env.js");
+// var api_key = require("./env.js");
 
 mongoose.connect('mongodb://localhost/rembr');
 
@@ -35,18 +35,21 @@ app.use(function (req, res, next) {
   });
 
 
-app.listen(7812);
+app.listen(7812, function() {
+  console.log(" *** listending on port 7812 *** ");
+});
 
+// Perspective Routes
 app.get("/", perspectivesController.index);
+app.get("/perspectives", perspectivesController.all);
+
+// User Routes
 app.get("/signup", usersController.getSignup);
 app.post("/signup", usersController.postSignup);
 app.get("/login", usersController.getLogin);
 app.post("/login", usersController.postLogin);
 app.get("/logout", usersController.getLogout);
-
 app.get("/users", usersController.index);
-
-app.get("/perspectives", perspectivesController.all);
 
 function authenticatedUser(req, res, next) {
     // If the user is authenticated, then we continue the execution
