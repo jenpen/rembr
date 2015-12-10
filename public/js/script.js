76f31830f03d47596075b3fe9a80fd6d3e1bc5b4
jen pengelly <jgpengelly@gmail.com>
2015-12-10 18:11:55 -0500

    if statement in delete

--- public/js/script.js
+++ public/js/script.js
@@ -3,30 +3,31 @@ $(document).ready(function(){
   Perspective.fetch().then(function(perspective){
     Perspective.all.forEach(function(perspective){
 
-        L.mapbox.featureLayer({
-          // this feature is in the GeoJSON format: see geojson.org for the full specification
-          type: 'Feature',
-          geometry: {
-            type: 'Point',
-            // coordinates here are in longitude, latitude order because x, y is the standard for GeoJSON and many formats
-            coordinates: [
-              perspective.longitude,
-              perspective.latitude
-            ]
-          },
-          properties: {
-            title: perspective.title + " "+ perspective.user,
-            description: perspective.text + "<button class='deletePerspective' title=" + perspective.id + ">Delete</button>",
-            // one can customize markers by adding simplestyle properties
-            // https://www.mapbox.com/guides/an-open-platform/#simplestyle
-            'marker-size': 'small',
-            'marker-color': '#FFA500',
-            'marker-symbol': 'marker-stroked'
-          }
-        }).addTo(map);
-      });
+      L.mapbox.featureLayer({
+        // this feature is in the GeoJSON format: see geojson.org for the full specification
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          // coordinates here are in longitude, latitude order because x, y is the standard for GeoJSON and many formats
+          coordinates: [
+            perspective.longitude,
+            perspective.latitude
+          ]
+        },
+        properties: {
+          title: perspective.title + " "+ perspective.user,
+          description: perspective.text + "<button class='deletePerspective' title=" + perspective.id + ">Delete</button>",
+          // one can customize markers by adding simplestyle properties
+          // https://www.mapbox.com/guides/an-open-platform/#simplestyle
+          'marker-size': 'small',
+          'marker-color': '#FFA500',
+          'marker-symbol': 'marker-stroked'
+        }
+      }).addTo(map);
     });
+  });
 
+  if($('#logout').html()){
     $("#map").on("click", ".deletePerspective", function(e){
       var perspectiveId = this.title;
       $.ajax({
@@ -36,6 +37,7 @@ $(document).ready(function(){
         location.reload();
       });
     });
+  }
 
   var popup = L.popup();
   function onMapClick(e) {
@@ -43,9 +45,11 @@ $(document).ready(function(){
     .setLatLng(e.latlng)
     .setContent(
       "<form class='form-group set-perspective-form' action='/' method='post'> Title: <input class='form-control' type ='text' name ='title'> Perspective: <textarea rows='4' class='form-control' id='perspective-input' name='text'></textarea><input id='field_show_none' type ='text' name='latitude' value="+e.latlng.lat+"><input id='field_show_none' type='text' name='longitude' value ="+e.latlng.lng+"> <input class='btn btn-default button-padding'  id='link-color' type='submit'></form>").openOn(map);
-  }
-
-  if($('#logout').html()){
-    map.on('click', onMapClick);
     }
+
+    if($('#logout').html()){
+      map.on('click', onMapClick);
+    }
+
+
   });
